201370eb58f848bae3984d61eba50102851d44bc
Connor Heraty <connor.heraty.work@gmail.com>
2021-03-29 17:47:02 -0400

    reconfigure celery.py comments

--- django_celery_example/celery.py
+++ django_celery_example/celery.py
@@ -6,18 +6,18 @@ from django.conf import settings
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_celery_example.settings")
 
-example_celery_app = Celery("django_celery_example")
+app = Celery("django_celery_example")
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-example_celery_app.config_from_object("django.conf:settings")
+app.config_from_object("django.conf:settings")
 
 # Load task modules from all registered Django app configs.
-example_celery_app.autodiscover_tasks(settings.INSTALLED_APPS)
+app.autodiscover_tasks(settings.INSTALLED_APPS)
 
-example_celery_app.conf.beat_schedule = {
+app.conf.beat_schedule = {
     # Executes every 2 minutes
     "query-twitter-api": {
         "task": "tweet.tasks.query_twitter_api",
